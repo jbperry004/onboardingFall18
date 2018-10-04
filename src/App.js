5d38f804b66391216862ae98d3f439f504307f8f
Jordan Perry <jordanperry@college.harvard.edu>
2018-10-04 12:54:55 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import ToDoContainer from "./components/ToDoContainer";
+
+class App extends Component {
+  render() {
+    return <ToDoContainer />;
+  }
+}
+
+export default App;
---
+++ src/components/ToDoContainer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class ToDoContainer extends Component {
+  render() {
+    return (
+      <div>Welcome to the onboarding project. Add your todo list here!</div>
+    );
+  }
+}
+
+export default ToDoContainer;
