2b8b6ea12cbae993fdc345fa02bc47bd86fb5043
ChrisMunene <munenechristoph@gmail.com>
2018-10-09 22:01:07 -0400

    Add created todos to array

--- src/components/ToDoContainer/index.js
+++ src/components/ToDoContainer/index.js
@@ -3,18 +3,35 @@ import React, {
 } from "react";
 
 class ToDoContainer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newTodo: '',
+      todos: []
+    }
+  }
+  newTodoChanged(event) {
+    this.setState({
+      newTodo: event.target.value
+    })
 
+  }
   formSubmitted(event) {
     event.preventDefault();
-    console.log("Form Submitted")
+    this.setState({
+      todos: [...this.state.todos, {
+        title: this.state.newTodo,
+        done: false
+      }]
+    })
   }
   render() {
     return (
       <div className="app">
         <h3>Todo List</h3>
-        <form onSubmit={this.formSubmitted}>
+        <form onSubmit={this.formSubmitted.bind(this)}>
           <label htmlFor='newTodo'>new Todo</label>
-          <input id="newTodo" name="newTodo" />
+          <input onChange={(event) => this.newTodoChanged(event)} id="newTodo" name="newTodo" />
           <button type="submit">Add Todo</button>
         </form>
       </div>
