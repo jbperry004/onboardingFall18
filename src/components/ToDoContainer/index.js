3e8513499b44e5fd07223fd364bd81005fa3cd9b
ChrisMunene <munenechristoph@gmail.com>
2018-10-10 19:41:06 -0400

    Replace bind with arrow functions

--- src/components/ToDoContainer/index.js
+++ src/components/ToDoContainer/index.js
@@ -1,75 +1,76 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 
-import NewTodoForm from './NewTodoForm';
-import TodoList from './TodoList';
+import NewTodoForm from "./NewTodoForm";
+import TodoList from "./TodoList";
 
 class ToDoContainer extends Component {
   constructor() {
     super();
     this.state = {
-      newTodo: '',
+      newTodo: "",
       todos: []
-    }
-
-    this.formSubmitted = this.formSubmitted.bind(this);
-    this.newTodoChanged = this.newTodoChanged.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
-    this.searchTodos = this.searchTodos.bind(this);
+    };
   }
 
   // Create new todo
-  newTodoChanged(event) {
+  newTodoChanged = event => {
     this.setState({
       newTodo: event.target.value
-    })
-
-  }
+    });
+  };
 
   // Form submission event handler
-  formSubmitted(event) {
+  formSubmitted = event => {
     event.preventDefault();
     // Add new todo to array
     this.setState({
-      newTodo: '',
-      todos: [...this.state.todos, {
-        title: this.state.newTodo,
-        done: false
-      }]
-    })
-  }
+      newTodo: "",
+      todos: [
+        ...this.state.todos,
+        {
+          title: this.state.newTodo,
+          done: false
+        }
+      ]
+    });
+  };
 
   // Remove clicked todolist
-  removeTodo(index) {
+  removeTodo = index => {
     const todos = [...this.state.todos];
     todos.splice(index, 1);
     this.setState({
       todos
-    })
-  }
+    });
+  };
 
   // Search todolist by substring
-  searchTodos(event) {
+  searchTodos = event => {
     let todos = [...this.state.todos];
-    todos = todos.filter(todo => todo.title.includes(event.target.value) === true)
+    todos = todos.filter(
+      todo => todo.title.includes(event.target.value) === true
+    );
     this.setState({
       todos
-    })
-  }
-
+    });
+  };
 
   render() {
     return (
       <div className="app">
         <h3>Todo List</h3>
         <NewTodoForm
-          newTodo={this.state.newTodo} formSubmitted={this.formSubmitted} newTodoChanged={this.newTodoChanged} />
+          newTodo={this.state.newTodo}
+          formSubmitted={this.formSubmitted}
+          newTodoChanged={this.newTodoChanged}
+        />
 
-        <TodoList todos={this.state.todos} removeTodo={this.removeTodo} searchTodos={this.searchTodos} />
-
+        <TodoList
+          todos={this.state.todos}
+          removeTodo={this.removeTodo}
+          searchTodos={this.searchTodos}
+        />
       </div>
-
     );
   }
 }
