426a17805ac0cf425d1d79c7856d04203140d8d5
ChrisMunene <munenechristoph@gmail.com>
2018-10-09 21:30:53 -0400

    Add html form for creating todos

--- src/components/ToDoContainer/index.js
+++ src/components/ToDoContainer/index.js
@@ -1,9 +1,23 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 
 class ToDoContainer extends Component {
+
+  formSubmitted(event) {
+    event.preventDefault();
+    console.log("Form Submitted")
+  }
   render() {
     return (
-      <div>Welcome to the onboarding project. Add your todo list here!</div>
+      <div className="app">
+        <h3>Todo List</h3>
+        <form onSubmit={this.formSubmitted}>
+          <label htmlFor='newTodo'>new Todo</label>
+          <input id="newTodo" name="newTodo" />
+          <button type="submit">Add Todo</button>
+        </form>
+      </div>
     );
   }
 }
