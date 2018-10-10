51c5a5f7e83387356321c3fb76c53b0c47c9dd83
ChrisMunene <munenechristoph@gmail.com>
2018-10-10 02:08:55 -0400

    remove form label

--- src/components/ToDoContainer/NewTodoForm.js
+++ src/components/ToDoContainer/NewTodoForm.js
@@ -3,7 +3,6 @@ import React from 'react';
 const NewTodoForm = (props) => {
     return (
         <form onSubmit={props.formSubmitted}>
-            <label htmlFor='newTodo'>new Todo</label>
             <input onChange={props.newTodoChanged} id="newTodo" name="newTodo" placeholder="Add todo" value={props.newTodo} />
             <button type="submit">Add Todo</button>
         </form>
