1396367033397bc5e32c43fff96859e9c73f57ce
ChrisMunene <munenechristoph@gmail.com>
2018-10-11 21:19:45 -0400

    Remove unnecessary return statements within arrow functions

--- src/components/ToDoContainer/NewTodoForm.js
+++ src/components/ToDoContainer/NewTodoForm.js
@@ -1,18 +1,16 @@
 import React from "react";
 
-const NewTodoForm = ({ formSubmitted, newTodoChanged, newTodo }) => {
-  return (
-    <form onSubmit={formSubmitted}>
-      <input
-        onChange={newTodoChanged}
-        id="newTodo"
-        name="newTodo"
-        placeholder="Add todo"
-        value={newTodo}
-      />
-      <button type="submit">Add Todo</button>
-    </form>
-  );
-};
+const NewTodoForm = ({ formSubmitted, newTodoChanged, newTodo }) => (
+  <form onSubmit={formSubmitted}>
+    <input
+      onChange={newTodoChanged}
+      id="newTodo"
+      name="newTodo"
+      placeholder="Add todo"
+      value={newTodo}
+    />
+    <button type="submit">Add Todo</button>
+  </form>
+);
 
 export default NewTodoForm;
